688dea904a9cedcde8158bb8fe03b0947e5e8505
GabrielXavier77 <gabrielxavier270723@gmail.com>
2025-08-04 19:54:49 -0300

    CardImage component added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
 import Welcome from "@/components/Welcome";
 import NavBar from "@/components/NavBar";
+import ImageCard from "@/components/ImageCard";
+
 export default function Home() {
-    return (
-        <div className="">
-            <NavBar
-                links={[
-                    {label: 'Início', href: '/'},
-                    {label: 'Produtos', href: '/products'},
-                    {label: 'Sobre Nós', href: '/about'}
-                ]}
-            />
-            <Welcome/>
-        </div>
-    );
+  return (
+    <div className="w-full h-full flex flex-col ">
+      <NavBar
+        links={[
+          { label: "Início", href: "/" },
+          { label: "Produtos", href: "/products" },
+          { label: "Sobre Nós", href: "/about" },
+        ]}
+      />
+      <div className="p-4">
+        <ImageCard
+          title="NextJS"
+          description="Next.js é um framework para aplicações web construídas 
+          com React que facilita o desenvolvimento de sites rápidos e otimizados.
+          Ele oferece funcionalidades como renderização do lado do servidor 
+          (SSR), geração de páginas estáticas (SSG), roteamento automático e 
+          suporte a APIs. É ideal para criar desde blogs simples até aplicações 
+          web complexas, com foco em performance, SEO e experiência do desenvolvedor."
+          imageUrl="nextjs-logo.svg"
+        />
+      </div>
+    </div>
+  );
 }
